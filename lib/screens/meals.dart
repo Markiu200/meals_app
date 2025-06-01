b1b8c1cb66c3c978694260e3409b52acb0bba958
markiu200 <markiu2007@interia.pl>
2025-06-01 12:23:58 +0200

    [exercise] Make MealItem lead to Meal screen
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37143992#learning-tools

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/widgets/meal_item.dart';
+import 'package:meals_app/screens/meal.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
+
+  void _selectMeal(BuildContext context, Meal meal) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (ctx) => MealScreen(meal: meal)),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +42,13 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (context, index) => MealItem(meal: meals[index]),
+        itemBuilder:
+            (context, index) => MealItem(
+              meal: meals[index],
+              onSelectMeal: () {
+                _selectMeal(context, meals[index]);
+              },
+            ),
       );
     }
 

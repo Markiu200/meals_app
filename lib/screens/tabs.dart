4979ae69267ab98d031247af71e9a15b3038c58b
markiu200 <markiu2007@interia.pl>
2025-06-03 15:00:00 +0200

    Make FiltersScreen accessible via MainDrawer
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37144018#learning-tools

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
@@ -48,6 +49,9 @@ class _TabsScreenState extends State<TabsScreen> {
 
   void _setScreen(String identifier) {
     if (identifier == 'filters') {
+      Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (ctx) => const FiltersScreen()));
     } else {
       // Close Drawer
       Navigator.of(context).pop();

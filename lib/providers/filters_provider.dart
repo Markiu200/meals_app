4e6d7fec7a032458bc8f06c1bcc065d42c7f5283
markiu200 <markiu2007@interia.pl>
2025-06-05 18:36:40 +0200

    Create FiltersNotifier to provide filters
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37144834#learning-tools

---
+++ lib/providers/filters_provider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  // Initialize with starting state
+  FiltersNotifier()
+    : super({
+        Filter.glutenFree: false,
+        Filter.lactoseFree: false,
+        Filter.vegetarian: false,
+        Filter.vegan: false,
+      });
+  void setFilter(Filter filter, bool isActive) {
+    // Spread operator works with maps too
+    state = {...state, filter: isActive};
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+      (ref) => FiltersNotifier(),
+    );

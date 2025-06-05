f727f257d0d40f5e38361b987cf53647148445da
markiu200 <markiu2007@interia.pl>
2025-06-05 14:15:36 +0200

    Providers: Use provider to get dummyMeals
    udemy: https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/learn/lecture/37144822#learning-tools

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:google_fonts/google_fonts.dart';
 //import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/tabs.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 final theme = ThemeData(
   useMaterial3: true,
   colorScheme: ColorScheme.fromSeed(
@@ -14,7 +16,8 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  // Wrap entire App in ProviderScope to unlock its functionality across the app
+  runApp(const ProviderScope(child: App()));
 }
 
 class App extends StatelessWidget {
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/models/meal.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
+import 'package:meals_app/providers/meals_provide.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Flutter convention of naming global variables with "k" prefix
 const kInitialFilters = {
@@ -14,16 +15,16 @@ const kInitialFilters = {
   Filter.vegan: false,
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
+  ConsumerState<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
   Map<Filter, bool> _selectedFilters = kInitialFilters;
@@ -75,8 +76,12 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
+    // ref allows to set up listeners to providers
+    // ref.watch() will rebuid build when data changes. Always preferred over read()
+    // that reads only once.
+    final meals = ref.watch(mealsProvider);
     final availableMeals =
-        dummyMeals.where((meal) {
+        meals.where((meal) {
           if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
             return false;
           }
